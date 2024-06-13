447118e93d3d19a653f4ab18f606881af74587a7
John D <johnd@Johns-MBP.attlocal.net>
2024-06-13 15:12:34 -0500

    new work experience added

--- src/App.jsx
+++ src/App.jsx
@@ -18,48 +18,44 @@ import Contact from './components/Contact'
 const technicalSkills = [
   {
     category: 'Test Management Tools',
-    skills: ["Jira", "Micro Focus ALM", "Axure Devops/TFS", "TestRail", "IBM RQM", "SCTM"]
+    skills: ["Jira"]
   },
   {
     category: 'Test Automation',
-    skills: ["Micro Focus UFT, Selenium, Tricentis Tosca, SilkTest, Worksoft Certify"]
+    skills: [" Cypress, Selenium, Playwright"]
   },
   {
-    category: 'Performance Testing Tools',
-    skills: ["LoadRunner, jMeter, Blazemeter, k6, Artillery"]
+    category: 'Database Skills',
+    skills: ["MySql, MongoDB"]
   },
   {
     category: 'API Testing Tools',
-    skills: ["PostMan, SoapUI, Python Requests"]
+    skills: ["PostMan, Insomnia"]
   },
   { 
-    category: 'Programming Languages',
-    skills: ['Java, JavaScript, VBScript, Python, C#, SQL, YAML, XML']
+    category: 'Languages',
+    skills: ['JavaScript, HTML, CSS, React ']
   },
   {
     category: 'Other',
-    skills: ['Jenkins, CircleCI, Serenity BDD, Cucumber, Gherkin, Maven, J/N/XUnit, RestAssured, Stryker, Snyk']
+    skills: ['XML, JSON']
   }
 ]
 const jobSummaries = [
   {
-    Position: "Senior Software Quality Engineer",
-    Date: "July 2020–Present",
-    company: 'Quicken Loans',
-    Location: "Detroit, MI",
+    Position: "Freelance Manual QA Tester",
+    Date: "April 2024-Present",
+    company: 'Self-employed',
+    Location: "online",
     Bullets: [
-      "Currently supporting 5 agile teams as the Train Quality Engineer with responsibilities that include setting the overall direction of the Quality Engineering program, architecting quality enablement processes and systems, devising and implementing testing strategies and tactics, setting quality-related goals, identifying key performance indicators, monitoring results, and adjusting as needed.",
-      "Built a Python Selenium automation framework from the ground-up with key features that include taking full-page screenshots, multiple browser and resolution support, and interrogating HTTP requests during test execution.",
-      "“Shifted-Left” on integration testing by leading efforts to design a new API testing framework which software and quality engineers now use to write and run integration tests on their local machine without having to first deploy their code changes to the test environment.",
-      "Developed Performance testing scenarios using k6 to enable performance testing in our CI/CD build pipelines as well as act as a sanity test before handing off our application to the Enterprise Performance Testing Team.",
-      "Currently designing an AI-backed, quality information system to gain telemetry on all test cases throughout the organization and act as a centralized hub for testing and quality metrics."
+       "Currently working for sevral companies such as Tester Work, Upwork, Utest as a freelance contractor where I perform funstional testing for clients on many types of websites."
     ]
   },
   {
-    Position: "Managing Quality Engineer",
-    Date: "April 2018–July 2020",
-    company: 'QualiTest Group',
-    Location: "Bridgewater, NJ",
+    Position: "Full Stack Coding Program",
+    Date: "December of 2023-April 2024",
+    company: 'University of Texas at Austin',
+    Location: "Austin, Texas",
     Bullets: [
       "On-site, hands-on consultant at client in Richmond, VA responsible for overall delivery of full software quality assurance lifecycle services as well as performing responsibilities of a QA Lead.",
       "Participated in all phases of quality assurance operations, including developing the overall test strategy for each given project, providing estimates to leadership, writing and executing test cases (ALM/UFT), managing KPIs, leading defect triage meetings, performing ALM Site Administration, and reporting progress to key stakeholders.",
@@ -69,10 +65,10 @@ const jobSummaries = [
     ]
   },
   {
-    Position: "Sr. Software Test Engineer",
-    Date: "May 2017–March 2018",
-    company: 'Fiserv',
-    Location: "Parsippany, NJ",
+    Position: "Manual QA Tester / Consultant",
+    Date: "May 2023-December 2023",
+    company: 'Precision Camera',
+    Location: "Austin, Texas",
     Bullets: [
       "Designed and executed tests for native iOS and Android mobile banking applications, web services, and web applications.",
       "Led efforts to design regression test suite for high profile applications, which included writing automated tests in SilkTest, reviewing code of junior team members, and supervising completion of projects.",
